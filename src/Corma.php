0020e83259d08d8b52438a4c99e6432573bb55db
Michael O'Connell <moconnell@lifestylepubs.com>
2016-02-25 11:09:01 -0600

    add convenience methods to Corma

--- src/Corma.php
+++ src/Corma.php
@@ -1,6 +1,7 @@
 <?php
 namespace Corma;
 
+use Corma\DataObject\DataObjectInterface;
 use Corma\Repository\ObjectRepositoryFactory;
 use Corma\Repository\ObjectRepositoryFactoryInterface;
 use Corma\Util\QueryHelper;
@@ -53,12 +54,76 @@ class Corma
     }
 
     /**
-     * @param $objectName
+     * @param string $objectName Object class with or without namespace
      * @return Repository\ObjectRepositoryInterface
      */
     public function getRepository($objectName)
     {
         return $this->repositoryFactory->getRepository($objectName);
+    }
+
+    /**
+     * Find an object by id
+     *
+     * @param string $objectName Object class with or without namespace
+     * @param string|int $id
+     * @return DataObjectInterface
+     */
+    public function find($objectName, $id)
+    {
+        return $this->getRepository($objectName)->find($id);
+    }
+
+    /**
+     * Find all of the specified object type
+     *
+     * @param string $objectName Object class with or without namespace
+     * @return DataObjectInterface[]
+     */
+    public function findAll($objectName)
+    {
+        return $this->getRepository($objectName)->findAll();
+    }
+
+    /**
+     * @param string $objectName Object class with or without namespace
+     * @param array $criteria column => value pairs
+     * @param array $orderBy column => order pairs
+     * @param int $limit Maximum results to return
+     * @param int $offset First result to return
+     * @return DataObjectInterface[]
+     */
+    public function findBy($objectName, array $criteria, array $orderBy = [], $limit = null, $offset = null)
+    {
+        return $this->getRepository($objectName)->findBy($criteria, $orderBy, $limit, $offset);
+    }
+
+    /**
+     * @param string $objectName Object class with or without namespace
+     * @param array $criteria column => value pairs
+     * @return DataObjectInterface
+     */
+    public function findOneBy($objectName, array $criteria)
+    {
+        return $this->getRepository($objectName)->findOneBy($criteria);
+    }
+
+    /**
+     * @param DataObjectInterface $object
+     * @return DataObjectInterface
+     */
+    public function save(DataObjectInterface $object)
+    {
+        return $this->getRepository($object->getClassName())->save($object);
+    }
+
+    /**
+     * @param DataObjectInterface $object
+     * @return void
+     */
+    public function delete(DataObjectInterface $object)
+    {
+        $this->getRepository($object->getClassName())->delete($object);
     }
 
     /**
--- test/CormaTest.php
+++ test/CormaTest.php
@@ -2,6 +2,9 @@
 namespace Corma\Test;
 
 use Corma\Corma;
+use Corma\Repository\ObjectRepositoryFactory;
+use Corma\Repository\ObjectRepositoryFactoryInterface;
+use Corma\Test\Fixtures\ExtendedDataObject;
 use Corma\Test\Fixtures\Repository\ExtendedDataObjectRepository;
 use Corma\Util\QueryHelper;
 use Doctrine\Common\Cache\ArrayCache;
@@ -35,6 +38,74 @@ class CormaTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf(ExtendedDataObjectRepository::class, $repository);
     }
 
+    public function testFind()
+    {
+        $mockRepo = $this->getMockBuilder(ExtendedDataObjectRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mockRepo->expects($this->once())->method('find')->with(5);
+
+        $this->getCorma($mockRepo)->find('ExtendedDataObject', 5);
+    }
+
+    public function testFindAll()
+    {
+        $mockRepo = $this->getMockBuilder(ExtendedDataObjectRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mockRepo->expects($this->once())->method('findAll');
+
+        $this->getCorma($mockRepo)->findAll('ExtendedDataObject');
+    }
+
+    public function testFindBy()
+    {
+        $mockRepo = $this->getMockBuilder(ExtendedDataObjectRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mockRepo->expects($this->once())->method('findBy')->with(['asdf'=>'value'], ['asdf'=>'ASC'], 2, 1);
+
+        $this->getCorma($mockRepo)->findBy('ExtendedDataObject', ['asdf'=>'value'], ['asdf'=>'ASC'], 2, 1);
+    }
+
+    public function testFindOneBy()
+    {
+        $mockRepo = $this->getMockBuilder(ExtendedDataObjectRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mockRepo->expects($this->once())->method('findOneBy')->with(['asdf'=>'value']);
+
+        $this->getCorma($mockRepo)->findOneBy('ExtendedDataObject', ['asdf'=>'value']);
+    }
+
+    public function testSave()
+    {
+        $mockRepo = $this->getMockBuilder(ExtendedDataObjectRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $object = new ExtendedDataObject();
+        $mockRepo->expects($this->once())->method('save')->with($object);
+
+        $this->getCorma($mockRepo)->save($object);
+    }
+
+    public function testDelete()
+    {
+        $mockRepo = $this->getMockBuilder(ExtendedDataObjectRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $object = new ExtendedDataObject();
+        $mockRepo->expects($this->once())->method('delete')->with($object);
+
+        $this->getCorma($mockRepo)->delete($object);
+    }
+
     /**
      * @depends testCreate
      * @param Corma $corma
@@ -42,5 +113,27 @@ class CormaTest extends \PHPUnit_Framework_TestCase
     public function testGetQueryHelper(Corma $corma)
     {
         $this->assertInstanceOf(QueryHelper::class, $corma->getQueryHelper());
+    }
+
+    /**
+     * @param $mockRepository
+     * @return Corma
+     */
+    protected function getCorma(\PHPUnit_Framework_MockObject_MockObject $mockRepository)
+    {
+        /** @var Connection $connection */
+        $connection = $this->getMockBuilder(Connection::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+
+        $mockFactory = $this->getMockBuilder(ObjectRepositoryFactory::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mockFactory->expects($this->once())->method('getRepository')->with('ExtendedDataObject')->willReturn($mockRepository);
+
+        /** @var ObjectRepositoryFactoryInterface $mockFactory */
+        return new Corma(new QueryHelper($connection, new ArrayCache()), $mockFactory);
     }
 }
