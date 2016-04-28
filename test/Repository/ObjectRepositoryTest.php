39da93ee331e16a31d9c88f36a46ccd700ffd977
michael <moconnell@lifestylepubs.com>
2016-04-28 10:52:11 -0500

    add unit tests to test that all events are being fired

--- test/Repository/ObjectRepositoryTest.php
+++ test/Repository/ObjectRepositoryTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Corma\Test\Repository;
 
+use Corma\DataObject\DataObjectEventInterface;
 use Corma\ObjectMapper;
 use Corma\Test\Fixtures\ExtendedDataObject;
 use Corma\Test\Fixtures\OtherDataObject;
@@ -11,13 +12,19 @@ use Corma\QueryHelper\QueryHelper;
 use Doctrine\Common\Cache\ArrayCache;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Platforms\MySqlPlatform;
+use Doctrine\DBAL\Query\QueryBuilder;
+use Doctrine\DBAL\Statement;
 use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class ObjectRepositoryTest extends \PHPUnit_Framework_TestCase
 {
     private $objectMapper;
     /** @var \PHPUnit_Framework_MockObject_MockObject */
     private $connection;
+
+    /** @var EventDispatcherInterface */
+    private $dispatcher;
 
     /** @var \PHPUnit_Framework_MockObject_MockObject */
     private $queryHelper;
@@ -31,6 +38,8 @@ class ObjectRepositoryTest extends \PHPUnit_Framework_TestCase
             return "`$column`";
         }));
         $this->connection->expects($this->any())->method('getDatabasePlatform')->willReturn(new MySqlPlatform());
+
+        $this->dispatcher = new EventDispatcher();
 
         $this->queryHelper = $this->getMockBuilder(QueryHelper::class)
             ->disableOriginalConstructor()
@@ -209,12 +218,237 @@ class ObjectRepositoryTest extends \PHPUnit_Framework_TestCase
         $this->getRepository()->deleteAll([$object]);
     }
 
+    public function testFindEvents()
+    {
+        $firedEvents = [
+            'DataObject.loaded' => 0,
+            'DataObject.ExtendedDataObject.loaded' => 0
+        ];
+        $this->dispatcher->addListener('DataObject.loaded', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.loaded'] ++;
+        });
+        $this->dispatcher->addListener('DataObject.ExtendedDataObject.loaded', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.ExtendedDataObject.loaded'] ++;
+        });
+
+        $mockQb = $this->getMockBuilder(QueryBuilder::class)->disableOriginalConstructor()
+            ->setMethods(['execute'])->getMock();
+
+        $mockStatement = $this->getMockBuilder(Statement::class)->disableOriginalConstructor()
+            ->setMethods(['fetch', 'setFetchMode'])->getMock();
+
+        $mockStatement->expects($this->once())->method('fetch')->willReturn(new ExtendedDataObject());
+
+        $mockQb->expects($this->once())->method('execute')->willReturn($mockStatement);
+
+        $this->queryHelper->expects($this->once())->method('buildSelectQuery')->willReturn($mockQb);
+
+        $repo = $this->getRepository();
+        $repo->find('234');
+
+        $this->assertEquals(1, $firedEvents['DataObject.loaded']);
+        $this->assertEquals(1, $firedEvents['DataObject.ExtendedDataObject.loaded']);
+    }
+
+    public function testFindAllEvents()
+    {
+        $firedEvents = [
+            'DataObject.loaded' => 0,
+            'DataObject.ExtendedDataObject.loaded' => 0
+        ];
+        $this->dispatcher->addListener('DataObject.loaded', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.loaded'] ++;
+        });
+        $this->dispatcher->addListener('DataObject.ExtendedDataObject.loaded', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.ExtendedDataObject.loaded'] ++;
+        });
+
+        $mockQb = $this->getMockBuilder(QueryBuilder::class)->disableOriginalConstructor()
+            ->setMethods(['execute'])->getMock();
+
+        $mockStatement = $this->getMockBuilder(Statement::class)->disableOriginalConstructor()
+            ->setMethods(['fetchAll'])->getMock();
+
+        $mockStatement->expects($this->once())->method('fetchAll')->willReturn([new ExtendedDataObject(), new ExtendedDataObject()]);
+
+        $mockQb->expects($this->once())->method('execute')->willReturn($mockStatement);
+
+        $this->queryHelper->expects($this->once())->method('buildSelectQuery')->willReturn($mockQb);
+
+        $repo = $this->getRepository();
+        $repo->findAll();
+
+        $this->assertEquals(2, $firedEvents['DataObject.loaded']);
+        $this->assertEquals(2, $firedEvents['DataObject.ExtendedDataObject.loaded']);
+    }
+
+    public function testSaveEvents()
+    {
+        $firedEvents = [
+            'DataObject.beforeSave' => 0,
+            'DataObject.ExtendedDataObject.beforeSave' => 0,
+            'DataObject.afterSave' => 0,
+            'DataObject.ExtendedDataObject.afterSave' => 0,
+            'DataObject.beforeInsert' => 0,
+            'DataObject.ExtendedDataObject.beforeInsert' => 0,
+            'DataObject.afterInsert' => 0,
+            'DataObject.ExtendedDataObject.afterInsert' => 0,
+            'DataObject.beforeUpdate' => 0,
+            'DataObject.ExtendedDataObject.beforeUpdate' => 0,
+            'DataObject.afterUpdate' => 0,
+            'DataObject.ExtendedDataObject.afterUpdate' => 0
+        ];
+
+        $this->dispatcher->addListener('DataObject.beforeSave', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.beforeSave'] ++;
+        });
+        $this->dispatcher->addListener('DataObject.ExtendedDataObject.beforeSave', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.ExtendedDataObject.beforeSave'] ++;
+        });
+
+        $this->dispatcher->addListener('DataObject.afterSave', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.afterSave'] ++;
+        });
+        $this->dispatcher->addListener('DataObject.ExtendedDataObject.afterSave', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.ExtendedDataObject.afterSave'] ++;
+        });
+
+        $this->dispatcher->addListener('DataObject.beforeInsert', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.beforeInsert'] ++;
+        });
+        $this->dispatcher->addListener('DataObject.ExtendedDataObject.beforeInsert', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.ExtendedDataObject.beforeInsert'] ++;
+        });
+
+        $this->dispatcher->addListener('DataObject.afterInsert', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.afterInsert'] ++;
+        });
+        $this->dispatcher->addListener('DataObject.ExtendedDataObject.afterInsert', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.ExtendedDataObject.afterInsert'] ++;
+        });
+
+        $this->dispatcher->addListener('DataObject.beforeUpdate', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.beforeUpdate'] ++;
+        });
+        $this->dispatcher->addListener('DataObject.ExtendedDataObject.beforeUpdate', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.ExtendedDataObject.beforeUpdate'] ++;
+        });
+
+        $this->dispatcher->addListener('DataObject.afterUpdate', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.afterUpdate'] ++;
+        });
+        $this->dispatcher->addListener('DataObject.ExtendedDataObject.afterUpdate', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.ExtendedDataObject.afterUpdate'] ++;
+        });
+
+        $repo = $this->getRepository();
+        $dataObject = new ExtendedDataObject();
+        $repo->save($dataObject);
+        $dataObject->setId('12345');
+        $repo->save($dataObject);
+
+        $this->assertEquals(2, $firedEvents['DataObject.beforeSave']);
+        $this->assertEquals(2, $firedEvents['DataObject.ExtendedDataObject.beforeSave']);
+        $this->assertEquals(2, $firedEvents['DataObject.afterSave']);
+        $this->assertEquals(2, $firedEvents['DataObject.ExtendedDataObject.afterSave']);
+
+        $this->assertEquals(1, $firedEvents['DataObject.beforeInsert']);
+        $this->assertEquals(1, $firedEvents['DataObject.ExtendedDataObject.beforeInsert']);
+        $this->assertEquals(1, $firedEvents['DataObject.afterInsert']);
+        $this->assertEquals(1, $firedEvents['DataObject.ExtendedDataObject.afterInsert']);
+
+        $this->assertEquals(1, $firedEvents['DataObject.beforeUpdate']);
+        $this->assertEquals(1, $firedEvents['DataObject.ExtendedDataObject.beforeUpdate']);
+        $this->assertEquals(1, $firedEvents['DataObject.afterUpdate']);
+        $this->assertEquals(1, $firedEvents['DataObject.ExtendedDataObject.afterUpdate']);
+    }
+
+    public function testSaveAllEvents()
+    {
+        $firedEvents = [
+            'DataObject.beforeSave' => 0,
+            'DataObject.ExtendedDataObject.beforeSave' => 0,
+            'DataObject.afterSave' => 0,
+            'DataObject.ExtendedDataObject.afterSave' => 0,
+            'DataObject.beforeInsert' => 0,
+            'DataObject.ExtendedDataObject.beforeInsert' => 0,
+            'DataObject.afterInsert' => 0,
+            'DataObject.ExtendedDataObject.afterInsert' => 0,
+            'DataObject.beforeUpdate' => 0,
+            'DataObject.ExtendedDataObject.beforeUpdate' => 0,
+            'DataObject.afterUpdate' => 0,
+            'DataObject.ExtendedDataObject.afterUpdate' => 0
+        ];
+
+        $this->dispatcher->addListener('DataObject.beforeSave', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.beforeSave'] ++;
+        });
+        $this->dispatcher->addListener('DataObject.ExtendedDataObject.beforeSave', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.ExtendedDataObject.beforeSave'] ++;
+        });
+
+        $this->dispatcher->addListener('DataObject.afterSave', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.afterSave'] ++;
+        });
+        $this->dispatcher->addListener('DataObject.ExtendedDataObject.afterSave', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.ExtendedDataObject.afterSave'] ++;
+        });
+
+        $this->dispatcher->addListener('DataObject.beforeInsert', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.beforeInsert'] ++;
+        });
+        $this->dispatcher->addListener('DataObject.ExtendedDataObject.beforeInsert', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.ExtendedDataObject.beforeInsert'] ++;
+        });
+
+        $this->dispatcher->addListener('DataObject.afterInsert', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.afterInsert'] ++;
+        });
+        $this->dispatcher->addListener('DataObject.ExtendedDataObject.afterInsert', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.ExtendedDataObject.afterInsert'] ++;
+        });
+
+        $this->dispatcher->addListener('DataObject.beforeUpdate', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.beforeUpdate'] ++;
+        });
+        $this->dispatcher->addListener('DataObject.ExtendedDataObject.beforeUpdate', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.ExtendedDataObject.beforeUpdate'] ++;
+        });
+
+        $this->dispatcher->addListener('DataObject.afterUpdate', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.afterUpdate'] ++;
+        });
+        $this->dispatcher->addListener('DataObject.ExtendedDataObject.afterUpdate', function(DataObjectEventInterface $event) use (&$firedEvents) {
+            $firedEvents['DataObject.ExtendedDataObject.afterUpdate'] ++;
+        });
+
+        $repo = $this->getRepository();
+        $dataObject = new ExtendedDataObject();
+        $dataObject->setId('12345');
+        $repo->saveAll([$dataObject, new ExtendedDataObject()]);
+
+        $this->assertEquals(2, $firedEvents['DataObject.beforeSave']);
+        $this->assertEquals(2, $firedEvents['DataObject.ExtendedDataObject.beforeSave']);
+        $this->assertEquals(2, $firedEvents['DataObject.afterSave']);
+        $this->assertEquals(2, $firedEvents['DataObject.ExtendedDataObject.afterSave']);
+
+        $this->assertEquals(1, $firedEvents['DataObject.beforeInsert']);
+        $this->assertEquals(1, $firedEvents['DataObject.ExtendedDataObject.beforeInsert']);
+        $this->assertEquals(1, $firedEvents['DataObject.afterInsert']);
+        $this->assertEquals(1, $firedEvents['DataObject.ExtendedDataObject.afterInsert']);
+
+        $this->assertEquals(1, $firedEvents['DataObject.beforeUpdate']);
+        $this->assertEquals(1, $firedEvents['DataObject.ExtendedDataObject.beforeUpdate']);
+        $this->assertEquals(1, $firedEvents['DataObject.afterUpdate']);
+        $this->assertEquals(1, $firedEvents['DataObject.ExtendedDataObject.afterUpdate']);
+    }
+
     /**
      * @return ExtendedDataObjectRepository
      */
     protected function getRepository()
     {
-        $repository = new ExtendedDataObjectRepository($this->connection, new EventDispatcher(), $this->objectMapper, new ArrayCache());
+        $repository = new ExtendedDataObjectRepository($this->connection, $this->dispatcher, $this->objectMapper, new ArrayCache());
         return $repository;
     }
 }
