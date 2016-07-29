535cb7c2389f14c184a021f739ad20923e2d34d7
michael <moconnell@lifestylepubs.com>
2016-07-29 09:32:40 -0500

    add unit of work class

---
+++ src/Util/UnitOfWork.php
@@ -0,0 +1,134 @@
+<?php
+namespace Corma\Util;
+
+use Corma\DataObject\DataObjectInterface;
+use Corma\ObjectMapper;
+
+/**
+ * Provides a way to safely execute any number of save / delete operations within a transaction.
+ *
+ * Can be used in two basic ways, one executeTransaction() will execute the provided callback within a transaction,
+ * or secondly you can add objects to save and delete, and call flush to save all objects.
+ */
+class UnitOfWork
+{
+    private $objectsToSave = [];
+    private $objectsToDelete = [];
+
+    /**
+     * @var ObjectMapper
+     */
+    private $orm;
+
+    public function __construct(ObjectMapper $orm)
+    {
+        $this->orm = $orm;
+    }
+
+    /**
+     * @param DataObjectInterface $object
+     * @return $this
+     */
+    public function save(DataObjectInterface $object)
+    {
+        $class = get_class($object);
+        if(isset($this->objectsToSave[$class])) {
+            $this->objectsToSave[$class][] = $object;
+        } else {
+            $this->objectsToSave[$class] = [$object];
+        }
+        return $this;
+    }
+
+    /**
+     * @param DataObjectInterface[] $objects
+     * @return $this
+     */
+    public function saveAll(array $objects)
+    {
+        foreach($objects as $object) {
+            $this->save($object);
+        }
+        return $this;
+    }
+
+    /**
+     * @param DataObjectInterface $object
+     * @return $this
+     */
+    public function delete(DataObjectInterface $object)
+    {
+        $class = get_class($object);
+        if(isset($this->objectsToDelete[$class])) {
+            $this->objectsToDelete[$class][] = $object;
+        } else {
+            $this->objectsToDelete[$class] = [$object];
+        }
+        return $this;
+    }
+
+    /**
+     * @param DataObjectInterface[] $objects
+     * @return $this
+     */
+    public function deleteAll(array $objects)
+    {
+        foreach($objects as $object) {
+            $this->delete($object);
+        }
+        return $this;
+    }
+
+    /**
+     * Wraps the passed function in a transaction and try / catch.
+     *
+     * If no exceptionHandler is passed the transaction will be rolled back and the exception rethrown.
+     *
+     * @param callable $run
+     * @param callable|null $exceptionHandler
+     * @throws \Exception
+     */
+    public function executeTransaction(callable $run, callable $exceptionHandler = null)
+    {
+        $db = $this->orm->getQueryHelper()->getConnection();
+        $db->beginTransaction();
+        try {
+            $run();
+            $db->commit();
+        } catch (\Exception $e) {
+            if($exceptionHandler) {
+                $exceptionHandler($e);
+            } else {
+                $db->rollBack();
+                throw $e;
+            }
+        }
+    }
+
+    /**
+     * Executes all operations
+     *
+     * @param callable $exceptionHandler
+     */
+    public function flush(callable $exceptionHandler = null)
+    {
+        $this->executeTransaction(function(){
+            foreach($this->objectsToSave as $objects) {
+                if(count($objects) == 1) {
+                    $this->orm->save($objects[0]);
+                } else {
+                    $this->orm->saveAll($objects);
+                }
+            }
+            foreach($this->objectsToDelete as $objects) {
+                if(count($objects) == 1) {
+                    $this->orm->delete($objects[0]);
+                } else {
+                    $this->orm->deleteAll($objects);
+                }
+            }
+        }, $exceptionHandler);
+        $this->objectsToSave = [];
+        $this->objectsToDelete = [];
+    }
+}
---
+++ test/Util/UnitOfWorkTest.php
@@ -0,0 +1,102 @@
+<?php
+namespace Util;
+
+use Corma\ObjectMapper;
+use Corma\QueryHelper\QueryHelper;
+use Corma\Test\Fixtures\ExtendedDataObject;
+use Corma\Test\Fixtures\OtherDataObject;
+use Corma\Util\UnitOfWork;
+use Doctrine\DBAL\Connection;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class UnitOfWorkTest extends \PHPUnit_Framework_TestCase
+{
+    private $objectMapper;
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    private $connection;
+
+    /** @var EventDispatcherInterface */
+    private $dispatcher;
+
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    private $queryHelper;
+
+    public function setUp()
+    {
+        $this->connection = $this->getMockBuilder(Connection::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->dispatcher = new EventDispatcher();
+
+        $this->queryHelper = $this->getMockBuilder(QueryHelper::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->queryHelper->expects($this->any())->method('getConnection')->willReturn($this->connection);
+
+        $this->objectMapper = $this->getMockBuilder(ObjectMapper::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->objectMapper->expects($this->any())->method('getQueryHelper')->willReturn($this->queryHelper);
+    }
+
+    public function testExecuteTransaction()
+    {
+        $unitOfWork = new UnitOfWork($this->objectMapper);
+        $this->connection->expects($this->once())->method('beginTransaction');
+        $this->connection->expects($this->once())->method('commit');
+        $unitOfWork->executeTransaction(function(){});
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testExecuteTransactionException()
+    {
+        $unitOfWork = new UnitOfWork($this->objectMapper);
+        $this->connection->expects($this->once())->method('beginTransaction');
+        $this->connection->expects($this->once())->method('rollback');
+        $unitOfWork->executeTransaction(function(){
+            throw new \Exception();
+        });
+    }
+
+    public function testExecuteTransactionExceptionCustomHandler()
+    {
+        $unitOfWork = new UnitOfWork($this->objectMapper);
+        $this->connection->expects($this->once())->method('beginTransaction');
+        $hasRun = false;
+        $unitOfWork->executeTransaction(function(){
+            throw new \Exception('My Message');
+        }, function(\Exception $e) use(&$hasRun){
+            $hasRun = true;
+            $this->assertEquals('My Message', $e->getMessage());
+        });
+        $this->assertTrue($hasRun);
+    }
+
+    public function testFlush()
+    {
+        $unitOfWork = new UnitOfWork($this->objectMapper);
+        $toSave = new ExtendedDataObject();
+        $toSave->setId(5);
+        $toSaveAll = [new OtherDataObject(), new OtherDataObject()];
+        $unitOfWork->save($toSave)
+            ->saveAll($toSaveAll);
+        $toDelete = new ExtendedDataObject();
+        $toDelete->setId(7);
+        $toDeleteAll = [new OtherDataObject(), new OtherDataObject()];
+        $unitOfWork->delete($toDelete)
+            ->deleteAll($toDeleteAll);
+        $this->connection->expects($this->once())->method('beginTransaction');
+        $this->connection->expects($this->once())->method('commit');
+        $this->objectMapper->expects($this->once())->method('save')->with($toSave);
+        $this->objectMapper->expects($this->once())->method('saveAll')->with($toSaveAll);
+        $this->objectMapper->expects($this->once())->method('delete')->with($toDelete);
+        $this->objectMapper->expects($this->once())->method('deleteAll')->with($toDeleteAll);
+        $unitOfWork->flush();
+    }
+}
